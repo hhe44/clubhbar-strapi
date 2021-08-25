11e986a1346e8d696503dd0484ff5dfca27e7b13
unknown <hhe@mailfence.com>
2021-08-25 02:10:54 -0400

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'eda0517013034a1a704f5b732346a363'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0b949640-fba1-4566-addc-5366ca0590a6'
+};
