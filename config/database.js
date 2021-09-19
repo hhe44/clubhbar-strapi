a5a8309156d8479fec907a77bb0189474dbbe4ab
unknown <hhe@mailfence.com>
2021-09-18 22:39:52 -0400

    revert local db config to sqlite

--- config/database.js
+++ config/database.js
@@ -1,19 +1,15 @@
 module.exports = ({ env }) => ({
-  defaultConnection: "default",
+  defaultConnection: 'default',
   connections: {
     default: {
-      connector: "bookshelf",
+      connector: 'bookshelf',
       settings: {
-        client: "postgres",
-        host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "postgres"),
-        username: env("DATABASE_USERNAME", "postgres"),
-        password: env("DATABASE_PASSWORD", "password"),
-        schema: env("DATABASE_SCHEMA", "public"), // Not Required
-        ssl: env('DATABASE_SSL', false),
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
-      options: {},
+      options: {
+        useNullAsDefault: true,
+      },
     },
   },
 });
